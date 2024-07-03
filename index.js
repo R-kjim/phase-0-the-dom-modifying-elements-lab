cbc9cdcc8c9c8f8e5191cf07a30bb10a68792fee
R-kjim <rkayjim@gmail.com>
2024-07-03 15:37:44 +0300

    modifying elements thru dom

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.removeChild(main)//deletes the main section which is a child of the body
+var newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.textContent='Alpha is the champion'
+document.body.append('newHeader')//newHeader variable points to a h1 element and is attached in the DOM
