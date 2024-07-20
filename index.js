b0f31bd3e1bae7fb11b68c98d5f1c7c095094a13
R-kjim <rkayjim@gmail.com>
2024-07-20 14:55:31 +0300

    modifying dom

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+// const element=document.createElement("div")
+// document.body.append(element)
+document.querySelector("#main").remove()
+let newHeader=document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.textContent='kjim is the champion'
